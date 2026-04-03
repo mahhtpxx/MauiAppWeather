aa6f725da115b470f51d494c5ad6708b4355fe1c
mahhtpx <maelianacleto19@gmail.com>
2026-04-02 23:31:45 -0300

    arumando bugs inesperados... senhor

--- MauiAppWeather/MainPage.xaml.cs
+++ MauiAppWeather/MainPage.xaml.cs
@@ -1,40 +1,36 @@
 ﻿using MauiAppWeather.Models;
 using MauiAppWeather.Services;
-using System.Threading.Tasks;
 
 namespace MauiAppWeather
 {
     public partial class MainPage : ContentPage
     {
-        int count = 0;
-
         public MainPage()
         {
             InitializeComponent();
         }
 
-        
-
         private async void Button_Clicked(object sender, EventArgs e)
         {
             try
             {
-                if(!string.IsNullOrEmpty(txt_cidade.Text))
+                if (!string.IsNullOrEmpty(txt_cidade.Text))
                 {
                     Tempo? t = await DataService.Getprevisao(txt_cidade.Text);
 
-                    if (t != null) 
+                    if (t != null)
                     {
-                        string dados_previsao = "";
+                        string dados_previsao =
+                            $"Latitude: {t.lat}\n" +
+                            $"Longitude: {t.lon}\n" +
+                            $"Nascer do sol: {t.sunrise}\n" +
+                            $"Pôr do sol: {t.sunset}\n" +
+                            $"Temp Máx: {t.temp_max}\n" +
+                            $"Temp Mín: {t.temp_min}\n";
 
-                        dados_previsao = $"Latitude: {t.lat}\n" +
-                                         $"Longitude: {t.lon}\n" +
-                                         $"Nascer do sol: {t.sunrise}\n" +
-                                         $"Pôr do sol: {t.sunset}\n" +
-                                         $"Temp Máx: {t.temp_max} \n" +
-                                         $"Temp Mín: {t.temp_min} \n";
-
-                    } else
+                        lbl_res.Text = dados_previsao;
+                    }
+                    else
                     {
                         lbl_res.Text = "Sem dados de previsão do tempo";
                     }
@@ -46,9 +42,8 @@ namespace MauiAppWeather
             }
             catch (Exception ex)
             {
-                DisplayAlert("Ops...", ex.Message, "OK");
+                await DisplayAlert("Ops...", ex.Message, "OK");
             }
         }
     }
-
 }
